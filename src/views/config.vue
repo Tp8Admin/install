3568d08afb63e410fa704c8e2f5dbdc956d2c732
YangHongwei <403236160@qq.com>
2023-08-20 22:44:25 +0800

    更改配置页面

--- src/views/config.vue
+++ src/views/config.vue
@@ -1,319 +1,195 @@
-<template>
-    <div class="container">
-        <div class="table-title">站点配置</div>
-        <div class="table">
-           
-            <el-form ref="formRef" label-width="150px" :rules="rules" :model="state.formItem">
-                <!-- 表单 -->
-                <transition-group name="slide-bottom">
-                    <div v-for="(item, idx) in state.formItem" :key="idx">
-                        <div v-if="item.type == 'br'" class="table-item-br"></div>
-                        <div v-else class="table-column table-item">
-                            <el-form-item :prop="item.name" class="table-label" :label="item.label">
-                                <el-input
-                                    :placeholder="item.placeholder ? item.placeholder : ''"
-                                    v-model="item.value"
-                                    class="table-input"
-                                    :type="item.type"
-                                ></el-input>
-                                <div class="block-help" v-if="item.blockHelp">{{ item.blockHelp }}</div>
-                            </el-form-item>
-                        </div>
-                    </div>
-                </transition-group>
 
-                <!-- 按钮 -->
-                <transition name="slide-bottom">
-                   
-                    <div class="footer-buttons">
-                        <el-button class="button">{{ t('Previous step') }}</el-button>
-                        <el-button type="primary" class="button">
-                            {{ t('Install now') }}
-                        </el-button>
-                    </div>
-                </transition>
-            </el-form>
-        </div>
-    </div>
-</template>
-
-<script setup lang="ts">
+<script lang="ts" setup>
 /**
  * 一：依赖部分
  */
-import { reactive } from 'vue'
-import { useI18n } from 'vue-i18n' // 引用国际化
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'element-plus' // 表单实例类型
+import { useCommon } from '@/stores/common'
 
 /**
  * 二：定义部分
  */
-const { t } = useI18n() // 国际化
+// 表单实例
+const ruleFormRef = ref<FormInstance>()
 
-const state = reactive({
-    formItem: {
-        /**
-         * 第一部分：数据库信息
-         */
-        // 数据库地址
-        hostname: {
-            label: t('Mysql database address'),
-            value: '127.0.0.1',
-            name: 'hostname',
-            type: 'text',
-        },
-        // 用户名
-        username: {
-            label: t('MySQL connection user name'),
-            value: 'root',
-            name: 'username',
-            type: 'text',
-        },
-        // 密码
-        password: {
-            label: t('MySQL connection password'),
-            value: '',
-            name: 'password',
-            type: 'password',
-        },
-        // 数据库名
-        database: {
-            label: t('Mysql database name'),
-            value: '',
-            name: 'database',
-            type: 'text',
-            blockHelp: '',
-        },
-        // 数据表前缀
-        prefix: {
-            label: t('MySQL data table prefix'),
-            value: 'ta_',
-            name: 'prefix',
-            type: 'text',
-        },
-        
-        /**
-         * 第二部分：管理员信息
-         */
-        // 管理员用户名
-        adminname: {
-            label: t('Administrator user name'),
-            value: 'admin',
-            name: 'adminname',
-            type: 'text',
-        },
-        // 管理员密码
-        adminpassword: {
-            label: t('Administrator password'),
-            value: '',
-            name: 'adminpassword',
-            type: 'password',
-            placeholder: t('Backend login password'),
-        },
-        // 管理员密码
-        repeatadminpassword: {
-            label: t('Duplicate administrator password'),
-            value: '',
-            name: 'repeatadminpassword',
-            type: 'password',
-        },
-        
-        /**
-         * 第三部分：站点信息
-         */
-        // 站点名称
-        sitename: {
-            label: t('Site name'),
-            value: 'Tp8Admin',
-            name: 'sitename',
-            type: 'text',
-        },
-    },
+// 公共数据
+const common = useCommon()
+
+// 表单数据
+const ruleForm = reactive({
+  /**
+   * 数据库配置
+   */
+  hostname: '127.0.0.1',
+  username: 'root',
+  password: '',
+  hostport: '3306',
+  database: '',
+  prefix: 'ta_',
+  /**
+   * 管理员配置
+   */
+  adminname: 'admin',
+  adminpassword: '',
+  repeatadminpassword: '',
+  /**
+   * 站点配置
+   */
+  sitename: 'Tp8Admin',
 })
 
+// 表单验证规则
+const rules = reactive({
+  /**
+   * 一：数据库部分
+   */
+  // MySQL数据库地址
+  hostname: [
+    { required: true, message: '请输入MySQL数据库地址', trigger: 'blur' },
+  ],
+  // MySQL数据库用户名
+  username: [
+    { required: true, message: '请输入MySQL数据库用户名', trigger: 'blur' },
+  ],
+  // MySQL数据库密码
+  password: [
+    { required: true, message: '请输入MySQL数据库密码', trigger: 'blur' },
+  ],
+  // MySQL数据库端口
+  hostport: [
+    { required: true, message: '请输入MySQL数据库端口', trigger: 'blur' },
+  ],
+  // MySQL数据库名称
+  database: [
+    { required: true, message: '请输入MySQL数据库名称', trigger: 'blur' },
+  ],
+  // MySQL数据库表前缀
+  prefix: [
+    { required: true, message: '请输入MySQL数据库表前缀', trigger: 'blur' },
+  ],
+  /**
+   * 二：管理员部分
+   */
+  // 管理员用户名
+  adminname: [
+    { required: true, message: '请输入管理员用户名', trigger: 'blur' },
+  ],
+  // 管理员密码
+  adminpassword: [
+    { required: true, message: '请输入管理员密码', trigger: 'blur' },
+  ],
+  // 重复管理员密码
+  repeatadminpassword: [
+    { required: true, message: '请重复管理员密码', trigger: 'blur' },
+  ],
+  /**
+   * 三：站点部分
+   */
+  // 站点名称
+  sitename: [
+    { required: true, message: '请输入站点名称', trigger: 'blur' },
+  ],
+})
+
+
+/**
+ * 三：方法部分
+ */
+// 上一步
+const prevStep = () => {
+  common.setStep('check')
+}
+// 提交表单
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
 </script>
 
+<template>
+  <div class="container">
+
+    <div class="table-title">第二步 站点配置</div>
+
+    <div class="table">
+      <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="180px">
+        <!-- MySQL数据库地址 -->
+        <el-form-item label="MySQL数据库地址" prop="hostname">
+          <el-input v-model="ruleForm.hostname" />
+        </el-form-item>
+        <!-- MySQL数据库用户名 -->
+        <el-form-item label="MySQL数据库用户名" prop="username">
+          <el-input v-model="ruleForm.username" />
+        </el-form-item>
+        <!-- MySQL数据库密码 -->
+        <el-form-item label="MySQL数据库密码" prop="password">
+          <el-input v-model="ruleForm.password" />
+        </el-form-item>
+        <!-- MySQL数据库端口 -->
+        <el-form-item label="MySQL数据库端口" prop="hostport">
+          <el-input v-model="ruleForm.hostport" />
+        </el-form-item>
+        <!-- MySQL数据库名称 -->
+        <el-form-item label="MySQL数据库名称" prop="database">
+          <el-input v-model="ruleForm.database" />
+        </el-form-item>
+        <!-- MySQL数据库表前缀 -->
+        <el-form-item label="MySQL数据库表前缀" prop="prefix">
+          <el-input v-model="ruleForm.prefix" />
+        </el-form-item>
+        <!-- 管理员账号 -->
+        <el-form-item label="管理员账号" prop="adminname">
+          <el-input v-model="ruleForm.adminname" />
+        </el-form-item>
+        <!-- 管理员密码 -->
+        <el-form-item label="管理员密码" prop="adminpassword">
+          <el-input v-model="ruleForm.adminpassword" />
+        </el-form-item>
+        <!-- 重复管理员密码 -->
+        <el-form-item label="重复管理员密码" prop="repeatadminpassword">
+          <el-input v-model="ruleForm.repeatadminpassword" />
+        </el-form-item>
+        <!-- 站点名称 -->
+        <el-form-item label="站点名称" prop="sitename">
+          <el-input v-model="ruleForm.sitename" />
+        </el-form-item>
+        <!-- 提交按钮 -->
+        <el-form-item>
+          <!-- 上一步 -->
+          <el-button @click="prevStep">上一步</el-button>
+          <el-button type="primary" @click="submitForm(ruleFormRef)">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+  </div>
+</template>
 
 
-<style scoped lang="scss">
-.phinx-fail-box {
-    display: block;
-    margin-top: 20px;
-    padding: 15px;
-    margin: 15px auto;
-    background-color: #fff;
-    border-radius: 4px;
-    .content-item {
-        line-height: 1.3;
-        border-radius: 4px;
-        padding: 10px;
-        background-color: #f5f5f5;
-        word-break: break-all;
-        font-family: Consolas, Monaco, Andale Mono, Ubuntu Mono, monospace;
-        margin: 15px 0;
-        .command {
-            line-height: 2;
-            font-weight: bold;
-        }
-        .block-help {
-            display: inline-block;
-            line-height: 2;
-            font-size: 13px;
-            color: #909399;
-        }
-        .text {
-            padding: 6px 0;
-            font-size: 14px;
-        }
-        .output-box {
-            position: relative;
-            border-radius: 5px;
-            box-shadow: #0005 0 2px 2px;
-            padding: 5px;
-            font-size: 13px;
-            background-color: #282c34;
-        }
-        .output {
-            color: #a9b7c6;
-        }
-        .mt10 {
-            margin-top: 10px;
-        }
-    }
-}
-.phinx-fail-footer-button {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
+<style lang="scss" scoped>
 .container {
-    margin-top: 10px;
-    .table-title {
-        display: block;
-        text-align: center;
-        font-size: 20px;
-        color: #303133;
-    }
-    .table {
-        max-width: 560px;
-        padding: 20px;
-        margin: 0 auto;
-        .table-item-br {
-            height: 20px;
-        }
-        .table-item:focus-within {
-            .table-input {
-                color: #303133;
-            }
-            border: 2px solid #4e73df;
-        }
-        .table-column {
-            padding: 12px;
-            border-radius: 3px;
-            border: 2px solid #fff;
-            transition: all 0.3s ease;
-        }
-        .table-error {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-bottom: 10px;
-            background-color: #f56c6c;
-            color: #fff;
-        }
-        .table-item {
-            display: flex;
-            align-items: center;
-            margin-bottom: 2px;
-            background-color: #fff;
-            color: #909399;
-            .table-label {
-                flex: 1;
-                font-size: 15px;
-                margin-bottom: 0;
-                .block-help {
-                    display: block;
-                    width: 100%;
-                    color: #909399;
-                    font-size: 13px;
-                    line-height: 16px;
-                    padding: 0 11px;
-                }
-            }
-        }
-    }
-    .footer-buttons {
-        margin-top: 20px;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        .button {
-            width: 50%;
-            height: 42px;
-        }
-    }
-    .connecting-prompt {
-        position: fixed;
-        top: 60px;
-        right: 100px;
-        font-size: 14px;
-        margin-top: 20px;
-        text-align: right;
-        color: #606266;
-    }
-    :deep(.el-input__wrapper) {
-        box-shadow: none;
-    }
-    :deep(.el-input__wrapper.is-focus) {
-        box-shadow: none;
-    }
-    :deep(.el-form-item.is-error .el-input__wrapper) {
-        box-shadow: none;
-    }
-    :deep(.el-form-item__error) {
-        left: 11px;
-        margin-top: -6px;
-    }
-    :deep(.el-input__inner) {
-        line-height: 29px;
-    }
-}
-.install-tips-box {
-    padding: 0 20px;
-    .install-tips-close {
-        position: absolute;
-        width: 22px;
-        height: 22px;
-        top: -11px;
-        right: -11px;
-        border: 1px solid #d50600;
-        border-radius: 50%;
-    }
-    .install-tips {
-        position: relative;
-        padding: 10px;
-        background-color: #ffcdcd;
-        color: #d50600;
-        max-width: 570px;
-        margin: 20px auto 0 auto;
-        border-radius: 4px;
-        font-size: 14px;
-        .install-tips-title,
-        .install-tips-item {
-            text-indent: 1em;
-            background-color: #ffe5e5;
-            padding: 8px;
-            border-radius: 4px;
-            margin-bottom: 5px;
-        }
-        .install-tips-item:last-child {
-            margin-bottom: 0;
-        }
-    }
-    .change-route {
-        cursor: pointer;
-        color: #3f6ad8;
-    }
+  width: 500px;
+  margin: 0 auto;
+
+  .table-title {
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .table {
+    padding: 20px;
+    border: 1px solid #eee;
+    border-radius: 5px;
+  }
 }
 </style>
+```
